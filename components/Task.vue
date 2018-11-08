9de626606a111769026912e49a832318605b5ebe
hilarious23 <s.positex11@gmail.com>
2018-11-09 00:44:54 +0900

    create taskapp and display task and filter

---
+++ components/Task.vue
@@ -0,0 +1,29 @@
+[
+  {
+    id: 1,
+    name: 'buy milk',
+    done: false,
+    labelIds: [1, 2]
+  },
+  {
+    id: 2,
+    name: 'buy book',
+    done: true,
+    labelIds: [1,3]
+  }
+]
+
+[
+  {
+    id: 1,
+    text: 'shopping'
+  },
+  {
+    id: 2,
+    text: 'food'
+  },
+  {
+    id: 3,
+    text: 'book'
+  }
+]
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+
+<template lang="pug">
+  div
+    input(type="text" v-model="filteredWord")
+    ul
+      li(v-for="task in filteredTaskList" v-bind:key="task.id")
+        p {{ task.name }}
+        p {{ task.done }}
+        p {{ task.labels }}
+</template>
+
+<script>
+import task from '~/assets/data/task.json'
+
+export default {
+  data() {
+    return {
+      filteredWord: ''
+    }
+  },
+  computed: {
+    tasks() {
+      return task
+    },
+    filteredTaskList() {
+      const filtered = this.tasks.filter(task => {
+        return task.name.includes(this.filteredWord)
+      })
+      return filtered
+    }
+  }
+}
+</script>
