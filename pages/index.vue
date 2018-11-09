9c59b2c7cecba05c4dba5ee634435512efdfd371
hilarious23 <s.positex11@gmail.com>
2018-11-09 12:43:31 +0900

    label機能の実装とlabelfilter

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,39 @@
 <template lang="pug">
-  div タスク一覧
-    h2 
+  div
+    h2 タスク一覧 
     input(type="text" v-model="filteredWord")
     ul
       li(v-for="task in filteredTaskList" v-bind:key="task.id")
         //- task.doneがtrueならcheckedになる
         input(type="checkbox" v-bind:checked="task.done"
           v-on:change="toggleTaskStatus(task)")
-        p {{ task.name }}
+        span {{ task.name }}
+        span(v-for="id in task.labelIds" v-bind:key="id") {{ getLabelText(id)}}
     
     form(v-on:submit.prevent="addTask")
       input(type="text" v-model="newTaskName" placeholder="add new Task")
+
+    h2 ラベル一覧
+    ul
+      li(v-for="label in labels" v-bind:key="label.id")
+        //- checkされたらlabel.idがvalueになり、それがv-modelされnewTaskLabelIdsに保持される
+        input(type="checkbox" v-bind:value="label.id"
+          v-model="newTaskLabelIds")
+        span {{ label.text }}
+
+    form(v-on:submit.prevent="addLabel")
+      input(type="text" v-model="newLabelText" placeholder="new Label")
+
+    h2 ラベルでフィルタ
+    ul
+      li(v-for="label in labels" v-bind:key="label.id")
+        input(type="radio" v-bind:checked="label.id === filter"
+          v-on:change="changeFilter(label.id)")
+        span {{ label.text }}
+      li
+        input(type="radio" v-bind:checked="filter === null"
+          v-on:change="changeFilter(null)")
+        span フィルタしない
 </template>
 
 <script>
@@ -20,19 +43,29 @@ export default {
   data() {
     return {
       filteredWord: '',
-      newTaskName: ''
+      newTaskName: '',
+      newTaskLabelIds: [],
+      newLabelText: ''
     }
   },
   computed: {
+    // 直後のfilteredTaskListで使うようにtasksを取得
     tasks() {
       // storeのstateを読む
-      return this.$store.state.tasks
+      return this.$store.getters.filteredTasks
     },
     filteredTaskList() {
       const filtered = this.tasks.filter(task => {
         return task.name.includes(this.filteredWord)
       })
       return filtered
+    },
+    // getLabelTextで使う用にstate.labelsを取得
+    labels () {
+      return this.$store.state.labels
+    },
+    filter() {
+      return this.$store.state.filter
     }
   },
   methods: {
@@ -40,15 +73,38 @@ export default {
       // stateの更新なのでmutation→なのでcommit
       // addTaskmutationにわたすべきものは渡す
       this.$store.commit('addTask', {
-        name: this.newTaskName
+        name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
       })
       this.newTaskName = ''
+      this.newTaskLabelIds = []
     },
 
     // タスクの完了状態を更新する。なのでcommit
     toggleTaskStatus(task) {
       this.$store.commit('toggleTaskStatus', {
         id: task.id
+      })
+    },
+
+    addLabel () {
+      this.$store.commit('addLabel', {
+        text: this.newLabelText
+      })
+      this.newLabelText = ''
+    },
+
+    // liのtaskの中の更にtask.labelIdsをforループ
+    getLabelText(id) {
+      // toKOMAKI
+      const label = this.labels.filter(label => label.id === id)[0]
+      return label ? label.text : ''
+    },
+
+    // フィルタする対象のラベルを変更する
+    changeFilter (labelId) {
+      this.$store.commit('changeFilter', {
+        filter: labelId
       })
     }
   }
--- store/index.js
+++ store/index.js
@@ -6,24 +6,66 @@ export const state = () => ({
     {
       id: 1,
       name: 'buy milk',
+      labelIds: [1, 2],
       done: false,
     },
     {
       id: 2,
       name: 'buy book',
+      labelIds: [1, 3],
       done: true,
     }
   ],
-  // 次に追加するタスクのID、実際はコレは生成する
+
+  labels: [
+    {
+      id: 1,
+      text: '買い物'
+    },
+    {
+      id: 2,
+      text: '食料'
+    },
+    {
+      id: 3,
+      text: '本'
+    }
+  ],
+  // 次に追加するタスク,labelのID、実際はコレは生成する
   nextTaskId: 3,
+  nextLabelId: 4,
+  // フィルタするラベルのID初期値
+  filter: null
 })
 
+// gettersはstateから計算した値を返す処理
+export const getters = {
+  filteredTasks (state) {
+    // labelが選択されていなければそのままの一覧を返す
+    if (!state.filter) {
+      return state.tasks
+    }
+
+    // 選択されたlabelでフィルタリングする
+    // 各taskでtask.labelIdsにstate.filterがあるか検索
+    // あれば最初に見つかった位置のindexが返ってくる（つまりあるなら0以上)
+    // task.labelIds.indexOf(state.filter)が0以上になるtaskかを判断
+    // 条件を満たすstate.tasksをreturn
+    return state.tasks.filter(task => {
+      return task.labelIds.indexOf(state.filter) >= 0
+    })
+  }
+} 
+
+// mutationsはstateの更新
 export const mutations = {
-  addTask (state, { name }) {
+  // commitで受け取るものは{}で第二引数にしとく
+  addTask (state, { name, labelIds }) {
     // タスク追加。name: nameの省略
     state.tasks.push({
       id: state.nextTaskId,
       name,
+      labelIds,
       done: false
     })
     // 次に追加されるタスクのIDを更新
@@ -40,5 +82,21 @@ export const mutations = {
     filtered.forEach(task => {
       task.done = !task.done
     })
+  },
+
+  // labelの追加
+  // commitで受け取るものは{}で第二引数にしとくtext: textの略
+  addLabel(state, { text }) {
+    state.labels.push({
+      id: state.nextLabelId,
+      text
+    })
+    // 次に追加されるlabelのIDを更新
+    state.nextLabelId++
+  },
+
+  // フィルタリング対象のラベルを変更する
+  changeFilter (state, { filter }) {
+    state.filter = filter
   }
 }
